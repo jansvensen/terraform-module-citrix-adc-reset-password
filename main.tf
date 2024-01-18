5fea020086bdf6b58ac0068b75acb32f465d9046
Sven Jansen <github@jansvensen.de>
2024-01-18 15:45:26 +0100

    .

--- main.tf
+++ main.tf
@@ -24,6 +24,19 @@ resource "time_sleep" "pw_wait_a_few_seconds" {
 resource "citrixadc_nsconfig_save" "pw_save" {    
     all       = true
     timestamp = timestamp()
+    
+    # Will not error when save is already in progress
+    concurrent_save_ok = true
+
+    # Set to non zero value to retry the save config operation
+    # Will throw error if limit is surpassed
+    concurrent_save_retries = 3
+
+    # Time interval between save retries
+    concurrent_save_interval = "10s"
+
+    # Total timeout for all retries
+    concurrent_save_timeout = "1m"
 
   depends_on = [
     time_sleep.pw_wait_a_few_seconds
