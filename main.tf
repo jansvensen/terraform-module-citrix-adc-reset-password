a9f7de1de99e5b087dbb0ca24db646ea62ce8646
Sven Jansen <github@jansvensen.de>
2024-01-18 15:57:55 +0100

    -

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #####
 resource "citrixadc_password_resetter" "pw_password_reset" {
   username     = var.adc-base.username
-  password     = 'nsroot'
+  password     = "nsroot"
   new_password = var.adc-base.password
 }
 
