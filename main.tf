2e7494a92afac12aa688c5a10ae14c0fd5d0baa2
Sven Jansen <github@jansvensen.de>
2024-01-18 16:00:07 +0100

    .

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #####
 resource "citrixadc_password_resetter" "pw_password_reset" {
   username     = var.adc-base.username
-  password     = "nsroot"
+  password     = var.adc-base.oldpassword
   new_password = var.adc-base.password
 }
 
@@ -24,19 +24,6 @@ resource "time_sleep" "pw_wait_a_few_seconds" {
 resource "citrixadc_nsconfig_save" "pw_save" {    
     all       = true
     timestamp = timestamp()
-    
-    # Will not error when save is already in progress
-    concurrent_save_ok = true
-
-    # Set to non zero value to retry the save config operation
-    # Will throw error if limit is surpassed
-    concurrent_save_retries = 3
-
-    # Time interval between save retries
-    concurrent_save_interval = "10s"
-
-    # Total timeout for all retries
-    concurrent_save_timeout = "1m"
 
   depends_on = [
     time_sleep.pw_wait_a_few_seconds
